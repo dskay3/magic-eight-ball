0e5f31bdce9bed54af8a323e576b0fc6c8e25a81
Daniel Kim <dskay7@gmail.com>
2018-03-30 23:33:36 -0400

    added functionality and UI to app

--- magic eight ball/ViewController.swift
+++ magic eight ball/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let ballArr = ["answer yes ball", "ask me again ball", "no ball", "no idea ball", "yes ball"]
+    var ballIndex : Int = 1
+    
+    @IBOutlet weak var crystalBallImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        generateAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        generateAnswer()
+    }
+    
+    // generates a new ball view image
+    func generateAnswer() {
+        ballIndex = Int(arc4random_uniform(5))
+        crystalBallImage.image = UIImage(named: ballArr[ballIndex])
+    }
 }
 
